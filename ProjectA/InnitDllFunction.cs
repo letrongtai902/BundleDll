feb015dde60803cc24a46667437f82cdb961bf60
Lê Trọng Tài <62075154+letrongtai902@users.noreply.github.com>
2021-04-12 16:38:48 +0700

    Comit 1st || upcode

---
+++ ProjectA/InnitDllFunction.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectA
+{
+    public class InnitDllFunction
+    {
+        public int Add3Number(int a, int b, int c)
+        {
+            return a + b + c;
+        }
+    }
+}
---
+++ ProjectD/TestFuncD.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectD
+{
+    public class TestFuncD
+    {
+        public int Count(int a, int b )
+        {
+            int count = 0;
+            for(int i = a;i<b;i++)
+            {
+                count += i;
+            }
+            return count;
+        }
+    }
+}
